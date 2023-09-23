b6b331a8c209959aa3aef3e68f80810394065998
geovanasilva <sts.geovana@gmail.com>
2023-09-23 17:21:09 -0300

    feat: pro and user person pages

---
+++ src/pages/profissional-person.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+import profissional from "@/assets/profissional.svg";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Desafio Lacrei</title>
+        <meta
+          name="description"
+          content="Site desenvolvido para concorrer a uma vaga no Lacrei Saúde"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Header />
+      <Main
+        title="Profissional"
+        isHomePage={false}
+        image={profissional}
+        alt=""
+      />
+      <Footer />
+    </>
+  );
+}
---
+++ src/pages/user-person.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+import guysTypingMobile from "@/assets/guys-typing-mobile.svg";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Desafio Lacrei</title>
+        <meta
+          name="description"
+          content="Site desenvolvido para concorrer a uma vaga no Lacrei Saúde"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Header />
+      <Main
+        title="Pessoa Usuária"
+        isHomePage={false}
+        image={guysTypingMobile}
+        alt=""
+      />
+      <Footer />
+    </>
+  );
+}
